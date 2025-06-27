4f3f1902c29591fea4b8488dc3c76dde35e4d4f5
zi9629@partner.kit.edu <ka_zi9629@uc3n991.localdomain>
2025-06-27 08:23:58 +0200

    empyt

--- graph_hdc/experiments/hypernet_reconstruct/_5_recon_grid_search_four_only.sh
+++ graph_hdc/experiments/hypernet_reconstruct/_5_recon_grid_search_four_only.sh
@@ -2,7 +2,7 @@
 
 PROJECT_DIR="/home/ka/ka_iti/ka_zi9629/projects/graph_hdc"
 EXPERIMENTS_PATH="${PROJECT_DIR}/graph_hdc/experiments/hypernet_reconstruct"
-SCRIPT="${EXPERIMENTS_PATH}/04_hypernet_reconstruct_multiple_corrected.py"
+SCRIPT="${EXPERIMENTS_PATH}/04_hypernet_reconstruct_multiple_corrected_part2.py"
 
 DATASET="ZINC_ND_COMB"
 SEED="42"
@@ -19,12 +19,12 @@ for hv_dim in "${hv_dims[@]}"; do
   for vsa in "${VSAs[@]}"; do
     sbatch \
       --job-name=hyperrec \
-      --partition=gpu_mi300 \
-      --time=20:00:00 \
+      --partition=gpu_h100_il \
+      --time=28:00:00 \
       --gres=gpu:1 \
       --nodes=1 \
       --ntasks=1 \
-      --mem=64G \
+      --mem=32G \
       --wrap="module load devel/cuda/11.8 && \\
         pixi run python ${SCRIPT} \\
           --PROJECT_DIR=\"\\\"${PROJECT_DIR}\\\"\" \\
