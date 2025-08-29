fedfa319049df8503bb740efbb5170fcc28fab2b
Kaveh, Arvand (FDP4_EXTERN) <extern.arvand.kaveh@porsche.de>
2025-08-29 14:27:05 +0200

    added ZincPairsV2 bash script

--- src/exp/dataset_generation/_2_generate_zinc_pairs_2.sh
+++ src/exp/dataset_generation/_2_generate_zinc_pairs_2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-# bwUniCluster 3.0 — standard CPU node job
-# Regular CPU queue: `cpu` (72h max). Dev queue `dev_cpu` is limited to ~30 min.  # docs
-#
+# bwUniCluster 3.0 — single-GPU dev job
+# Partitions:
+# cpu_il dev_cpu_il | cpu dev_cpu | highmem dev_highmem | gpu_h100 dev_gpu_h100 | gpu_mi300 | gpu_a100_il gpu_h100_il|
 
 #SBATCH --job-name=ZincPairsV2
 #SBATCH --partition=cpu          # standard CPU partition (no GPUs)
-#SBATCH --time=11:00:00          # reasonable walltime (adjust as needed)
+#SBATCH --time=24:00:00          # reasonable walltime (adjust as needed)
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8        # 8 CPU cores for your Python job
@@ -15,7 +15,7 @@
 module load devel/cuda/11.8
 
 # ========== Experiment Path Configuration ==========
-PROJECT_DIR="/home/ka/ka_iti/ka_zi9629/projects/graph_hdc"
+PROJECT_DIR="${GHDC_HOME}"
 EXPERIMENTS_PATH="${PROJECT_DIR}/src/exp/dataset_generation"
 SCRIPT_NAME="generate_zinc_pairs.py"
 SCRIPT="${EXPERIMENTS_PATH}/${SCRIPT_NAME}"
