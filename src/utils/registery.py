4ded1a93a6b3d2723af6362c0db36b90201213b1
Arvand Kaveh <arvand.kaveh@tuta.com>
2025-09-23 20:19:40 +0200

    real nvp hpo ready

--- src/utils/registery.py
+++ src/utils/registery.py
@@ -1,31 +1,38 @@
-# ------------------------- registry -------------------------
+from __future__ import annotations
+
 from collections.abc import Callable
-from typing import Literal
+from typing import Any, Literal
 
 import pytorch_lightning as pl
 from pytorch_lightning import LightningModule
 
 ModelType = Literal["MLP", "BAH", "GIN-F", "GIN-C", "GIN-LF", "NVP"]
 
-_MODEL_REGISTRY: dict[ModelType, Callable[..., pl.LightningModule]] = {}
+# Store CLASSES (constructors), not instances
+_MODEL_REGISTRY: dict[ModelType, type[LightningModule]] = {}
 
 
-def register_model(name: ModelType):
-    def deco(ctor: Callable[..., pl.LightningModule]):
+def register_model(name: ModelType) -> Callable[[type[LightningModule]], type[LightningModule]]:
+    """Decorator: register a LightningModule subclass under `name`."""
+    def deco(ctor: type[LightningModule]) -> type[LightningModule]:
         _MODEL_REGISTRY[name] = ctor
         return ctor
-
     return deco
 
 
-def resolve_model(name: ModelType, **kwargs) -> pl.LightningModule:
-    if name not in _MODEL_REGISTRY:
+def resolve_model(name: ModelType, **kwargs: Any) -> pl.LightningModule:
+    """Instantiate the registered class with kwargs."""
+    cls = _MODEL_REGISTRY.get(name)
+    if cls is None:
         msg = f"Unknown model '{name}'. Registered: {list(_MODEL_REGISTRY)}"
         raise KeyError(msg)
-    return _MODEL_REGISTRY[name](**kwargs)
+    return cls(**kwargs)
 
-def retrieve_model(name: ModelType) -> Callable[LightningModule]:
-    if name not in _MODEL_REGISTRY:
+
+def retrieve_model(name: ModelType) -> type[LightningModule]:
+    """Return the registered class (not instantiated)."""
+    cls = _MODEL_REGISTRY.get(name)
+    if cls is None:
         msg = f"Unknown model '{name}'. Registered: {list(_MODEL_REGISTRY)}"
         raise KeyError(msg)
-    return _MODEL_REGISTRY[name]
+    return cls
