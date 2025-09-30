ada1f9f652d0e812785ef7e68247be57d1a6d7fc
Arvand Kaveh <arvand.kaveh@tuta.com>
2025-09-30 15:53:19 +0200

    hard pool resampler

---
+++ src/exp/classification_v5_hard_pool_resampler/__2_bah_medium_qm9.sh
@@ -0,0 +1,17 @@
+# ---- BAH: MEDIUM (adds 2-layer projection; moderate dropout) ----
+JOB_NAME=BAH_med_qm9 \
+PROJ_DIM=1024 \
+N_HEADS=8 \
+PROJ_hidden=2048 \
+SHARE_PROJ=False \
+DROPOUT=0.10 \
+NORM=True \
+USE_LAYERNORM=True \
+USE_TEMPERATURE=True \
+HV_DIM=1600 \
+DATASET=QM9_SMILES_HRR_1600 \
+VSA=HRR \
+BATCH_SIZE=512 \
+LR=1.0e-3 \
+EPOCHS=30 \
+./_2_bah_lightning.sh
