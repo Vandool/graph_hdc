5b9e704a541fe2464cf187324e92e0fdc3246fb8
Arvand Kaveh <arvand.kaveh@tuta.com>
2025-10-17 10:37:17 +0200

    graph decoding ablations

--- src/datasets/sa_score.py
+++ src/datasets/sa_score.py
@@ -1,33 +1,40 @@
-from rdkit import Chem
-from rdkit.Chem import rdMolDescriptors
+import gzip
 import math
 import pickle
-import gzip
+
+from rdkit import Chem
+from rdkit.Chem import rdMolDescriptors
 
 from src.utils.utils import GLOBAL_DATASET_PATH
 
 
 # Load the fragment scores
-def readFragmentScores(name=GLOBAL_DATASET_PATH / 'sa_score/fpscores.pkl.gz'):
-    with gzip.open(name, 'rb') as f:
-        data = pickle.load(f, encoding='latin1')
+def readFragmentScores(name=GLOBAL_DATASET_PATH / "sa_score/fpscores.pkl.gz"):
+    with gzip.open(name, "rb") as f:
+        data = pickle.load(f, encoding="latin1")
     outDict = {}
     for i in data:
         outDict[i[0]] = float(i[1])
     return outDict
 
-fragmentScore = readFragmentScores()
+
+fragmentScore = None
+
 
 def numBridgeheadsAndSpiro(mol, ri=None):
     nSpiro = rdMolDescriptors.CalcNumSpiroAtoms(mol)
     nBridge = rdMolDescriptors.CalcNumBridgeheadAtoms(mol)
     return nBridge, nSpiro
 
+
 def calculateScore(m):
+    global fragmentScore
+    if fragmentScore is None:
+        fragmentScore = readFragmentScores()
     # fragment score
     fp = rdMolDescriptors.GetMorganFingerprint(m, 2)
     fps = fp.GetNonzeroElements()
-    score1 = 0.
+    score1 = 0.0
     nf = 0
     for bitId, v in fps.items():
         nf += v
@@ -49,30 +56,30 @@ def calculateScore(m):
     stereoPenalty = math.log10(nChiralCenters + 1)
     spiroPenalty = math.log10(nSpiro + 1)
     bridgePenalty = math.log10(nBridgeheads + 1)
-    macrocyclePenalty = 0.
+    macrocyclePenalty = 0.0
     if nMacrocycles > 0:
         macrocyclePenalty = math.log10(2)
 
-    score2 = 0. - sizePenalty - stereoPenalty - spiroPenalty - bridgePenalty - macrocyclePenalty
+    score2 = 0.0 - sizePenalty - stereoPenalty - spiroPenalty - bridgePenalty - macrocyclePenalty
 
     # correction for fingerprint density
     # not in the original publication, added in this version
-    score3 = 0.
+    score3 = 0.0
     if nAtoms > len(fps):
-        score3 = math.log(float(nAtoms) / len(fps)) * .5
+        score3 = math.log(float(nAtoms) / len(fps)) * 0.5
 
     sascore = score1 + score2 + score3
 
     # normalization
     min = -4.0
     max = 2.5
-    sascore = 11. - ((sascore - min + 1) / (max - min) * 9.)
+    sascore = 11.0 - ((sascore - min + 1) / (max - min) * 9.0)
     # smooth the 10-end
-    if sascore > 8.:
-        sascore = 8. + math.log(sascore + 1. - 9.)
-    if sascore > 10.:
+    if sascore > 8.0:
+        sascore = 8.0 + math.log(sascore + 1.0 - 9.0)
+    if sascore > 10.0:
         sascore = 10.0
-    elif sascore < 1.:
+    elif sascore < 1.0:
         sascore = 1.0
 
     return sascore
