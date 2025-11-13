31963fb6a016e4cf3ea744abf176e800b2e31858
Arvand Kaveh <arvand.kaveh@tuta.com>
2025-11-13 22:06:00 +0100

    spline flow

--- src/exp/spline_flow/submit_sf.sh
+++ src/exp/spline_flow/submit_sf.sh
@@ -80,7 +80,7 @@ case "$CLUSTER" in
     MODULE_LOAD="module load devel/cuda"
     PIXI_ENV="cluster"
     [[ -z "${CPUS_PER_TASK:-}" ]] && CPUS_PER_TASK=16
-    TUPLES=$'normal|72:00:00|128G'
+    TUPLES=$'normal|12:00:00|128G'
     ;;
 esac
 
